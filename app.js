a8d087de7e4dccdc9c77eff4d16b41f02b51e939
Khokhlov Andrey <progeat@mail.ru>
2024-11-13 10:10:37 +0300

    Добавлены запросы к Post

--- app.js
+++ app.js
@@ -8,10 +8,18 @@ const {
   updateUser,
   deleteUser,
 } = require('./controllers/user');
+const {
+  getPost,
+  addPost,
+  editPost,
+  getPosts,
+  deletePost,
+} = require('./controllers/post');
 const mapUser = require('./helpers/mapUser');
 const authenticated = require('./middlewares/authenticated');
 const hasRole = require('./middlewares/hasRole');
 const ROLES = require('./constants/role');
+const mapPost = require('./helpers/mapPost');
 
 const port = 3005;
 const app = express();
@@ -47,7 +55,49 @@ app.post('/logout', (req, res) => {
   res.cookie('token', '', { httpOnly: true }).send({});
 });
 
-app.use(authenticated);
+app.get('/posts', async (req, res) => {
+  const { posts, lastPage } = await getPosts(
+    req.query.search,
+    req.query.limit,
+    req.query.page
+  );
+
+  res.send({ data: { lastPage, posts: posts.map(mapPost) } });
+});
+
+app.get('/posts/:id', async (req, res) => {
+  const post = await getPost(req.params.id);
+
+  res.send({ data: mapPost(post) });
+});
+
+app.use(authenticated); // Управление ролями
+
+app.post('/posts', hasRole([ROLES.ADMIN]), async (req, res) => {
+  const newPost = await addPost({
+    title: req.body.title,
+    content: req.body.content,
+    image: req.body.imageUrl,
+  });
+
+  res.send({ data: mapPost(newPost) });
+});
+
+app.patch('/posts/:id', hasRole([ROLES.ADMIN]), async (req, res) => {
+  const updatedPost = await editPost(req.params.id, {
+    title: req.body.title,
+    content: req.body.content,
+    image: req.body.imageUrl,
+  });
+
+  res.send({ data: mapPost(updatedPost) });
+});
+
+app.delete('/posts/:id', hasRole([ROLES.ADMIN]), async (req, res) => {
+  await deletePost(req.params.id);
+
+  res.send({ error: null });
+});
 
 app.get('/users', hasRole([ROLES.ADMIN]), async (req, res) => {
   const users = await getUsers();
@@ -84,5 +134,3 @@ mongoose
       console.log(`Server started on port ${port}`);
     });
   });
-
-// TODO удалить комментарий
--- models/Post.js
+++ models/Post.js
@@ -29,6 +29,6 @@ const PostSchema = mongoose.Schema(
   { timestamps: true }
 );
 
-const Post = mongoose.model('User', PostSchema);
+const Post = mongoose.model('Post', PostSchema);
 
 module.exports = Post;
